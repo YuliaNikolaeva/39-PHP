1522841a1fad15508a3eac2dbf50982e9ede7d60
Yulia Nikolaeva <y.nikolaeva@rndev.org>
2021-04-17 23:21:49 +0300

    HW-06

---
+++ HW-6/index.php
@@ -0,0 +1,59 @@
+<?php
+    session_start();
+
+
+    $products = [
+        2=>['name'=>'товар 1', 'price'=>100],
+        7=>['name'=>'товар 2', 'price'=>200],
+        43=>['name'=>'товар 3', 'price'=>300],
+        50=>['name'=>'товар 4', 'price'=>400],
+    ];
+
+    class Cart {
+        public $items;
+        public $sum;
+        public $discount;
+        public $count;
+        public $errors;
+
+        public function __construct() {
+            $this->items = [];
+            $this->sum = 0;
+            $this->$discount = 0;
+            $this->$count = 0;
+            $this->$errors = [];
+        }
+
+
+        public function addToExemplar ($id, $quantity) {
+            $product = $GLOBALS['products'][$id];
+
+             $this->items = [
+                $id => [
+                    'id'=> $id,
+                    'name' => $product['name'],
+                    'price'=> $product['price'],
+                    'quantity' => $quantity,
+                ]
+            ];
+        }
+
+
+        public function add ($id, $quantity) {
+            $product = $GLOBALS['products'][$id];
+
+             $_SESSION['cart']['items'][$id] = [
+                'id'=> $id,
+                'name' => $product['name'],
+                'price'=> $product['price'],
+                'quantity' => $quantity,
+            ];
+
+        }
+    }
+
+    $userCart = new Cart();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
---
+++ HW-6/list.php
@@ -0,0 +1,47 @@
+<?php
+     include 'index.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="styles.css" rel="stylesheet">
+    <title>Покупки</title>
+</head>
+<body>
+        <section class="cart">
+        <a href="./add.php" class="btn-standard">Магазин</a>
+        <h3 class='lead'>Корзина</h3>
+        <div class="cart-box">
+            <?php if(empty($userCart->items)) : ?>
+                <div class="empty-cart">Корзина пуста</div>
+            <?php endif; ?>
+
+            <?php if(!empty($userCart->items)) : ?>
+                <!-- <div class="cart-total">Всего к оплате: <span class="total"><?php echo $userCart->sum();?></span></div> -->
+                <ul class="cart-list">
+                    <li class="cart-item-head mb-sm">
+                        <div class="cart-item-name-head opacity-half item-head">Название</div>
+                        <div class="cart-item-cost-head opacity-half item-head">Цена, грн</div>
+                        <div class="cart-item-quantity-head opacity-half">Кол-во, шт.</div>
+                        <div class="cart-item-total-head opacity-half item-head">Сумма</div>
+                    </li>
+
+                    <?php foreach($_SESSION['cart']['items'] as $keyProduct => $product) {?>
+                    <li class="cart-item">
+                        <div class="cart-item-name"><?php echo $product['name']; ?></div>
+                        <div class="cart-item-cost"><?php echo $product['price']; ?></div>
+                        <div class="cart-item-quantity"><?php echo $product['quantity']; ?></div>
+                        <div class="cart-item-total"><?php echo $product['quantity'] * $product['price'] ?></div>
+                        <a href="/delete.php?id=<?php echo $product['id']; ?>">Удалить</a>
+                    </li>
+                    <?php } ?>
+                </ul>
+            <?php endif; ?>
+        </div>
+    </section>
+</body>
+</html>
